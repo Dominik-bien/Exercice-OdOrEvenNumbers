4324d2acd3eb4e4b98c2808d9953441c1a14c3d2
dombien <dombien23@gmail.com>
2025-02-23 10:56:24 +0100

    Program to check od or even numbers

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdOrEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program ask user to enter number 
+            Console.WriteLine("Enter a number: ");
+            var number = GetInput();
+            //if number mod 2 is equal to 0 the program displays that the number is even
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"Even Number. Number: {number}");
+            }
+            else
+            //else if number mod 2 is not equal to 0 the program displays that number is even
+            {
+                Console.WriteLine($"Odd number: Number: {number}");
+            }
+        }
+
+        //private method to throw exception if user enter a wrong format for the number variable.
+        private static int GetInput()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+                throw new Exception("The stated value is not correct");
+
+            return input;
+        }
+    }
+}
